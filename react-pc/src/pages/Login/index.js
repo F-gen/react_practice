30baa43b3996e47892d252c6ccc3a6648459fac3
eureka <1101853378@qq.com>
2022-10-11 22:13:16 +0800

    login

--- react-pc/src/pages/Login/index.js
+++ react-pc/src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import { Card } from 'antd'
+import { Card, Button, Checkbox, Form, Input } from 'antd'
 import logo from '@/assets/logo.png'
 import './index.scss'
 
@@ -8,6 +8,40 @@ const Login = () => {
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
+        <Form
+          validateTrigger={['onBlur', 'onChange']}
+        >
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号码格式不对',
+                validateTrigger: 'onBlur'
+              },
+              { required: true, message: '请输入手机号' }
+            ]}>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item >
+          <Form.Item name="code"
+            rules={[
+              { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur' },
+              { required: true, message: '请输入验证码' }
+            ]}>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
     </div>
   )
